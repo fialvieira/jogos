6804aad833b417fdcb77271be11165379b6a9f03
DESKTOP-O26PO1Q\fialv <fialvieira@gmail.com>
2022-10-04 16:17:24 -0300

    Introdução de laço para as rodadas.

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,20 +3,29 @@ print("Olá, bem vindo ao jogo de adivinhação!")
 print("**************************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute = input("Digite o seu número: ")
-acertou = int(chute) == numero_secreto
-maior   = int(chute) > numero_secreto
-menor   = int(chute) < numero_secreto
+while total_de_tentativas >= rodada:
+    print("Tentativa: " + str(rodada) + " de 3")
 
-print("Você digitou ", chute)
+    chute = input("Digite o seu número: ")
 
-if acertou:
-    print("Você acertou!")
-elif maior:
-    print("Você errou! O seu chute foi maior do que o número secreto.")
-elif menor:
-    print("Você errou! O seu chute foi menor do que o número secreto.")
+    acertou = int(chute) == numero_secreto
+    maior   = int(chute) > numero_secreto
+    menor   = int(chute) < numero_secreto
+
+    print("Você digitou ", chute)
+
+    if acertou:
+        print("Você acertou!")
+        total_de_tentativas = 0
+    elif maior:
+        print("Você errou! O seu chute foi maior do que o número secreto.")
+    elif menor:
+        print("Você errou! O seu chute foi menor do que o número secreto.")
+
+    rodada += 1
 
 print("**************************************")
 print("Fim de jogo!")
