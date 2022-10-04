6b25f9c97256edd822de1a154c84f37c55baa595
DESKTOP-O26PO1Q\fialv <fialvieira@gmail.com>
2022-10-04 15:51:35 -0300

    Melhoria na legibilidade do código.

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,14 +5,17 @@ print("**************************************")
 numero_secreto = 42
 
 chute = input("Digite o seu número: ")
+acertou = int(chute) == numero_secreto
+maior   = int(chute) > numero_secreto
+menor   = int(chute) < numero_secreto
 
 print("Você digitou ", chute)
 
-if numero_secreto == int(chute):
+if acertou:
     print("Você acertou!")
-elif int(chute) > numero_secreto:
+elif maior:
     print("Você errou! O seu chute foi maior do que o número secreto.")
-elif int(chute) < numero_secreto:
+elif menor:
     print("Você errou! O seu chute foi menor do que o número secreto.")
 
 print("**************************************")
