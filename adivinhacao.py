c71aeac1a13e35a20dfeb96c1462d050e7d57d05
DESKTOP-O26PO1Q\fialv <fialvieira@gmail.com>
2022-10-05 21:21:48 -0300

    Saída do laço com break.

--- adivinhacao.py
+++ adivinhacao.py
@@ -18,7 +18,7 @@ for rodada in range(1, total_de_tentativas + 1):
 
     if acertou:
         print("Você acertou!")
-        total_de_tentativas = 0
+        break
     elif maior:
         print("Você errou! O seu chute foi maior do que o número secreto.")
     elif menor:
