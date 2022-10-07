c57a1bf1dc4be881d132436ae4430b8f8153d762
DESKTOP-O26PO1Q\fialv <fialvieira@gmail.com>
2022-10-07 15:40:54 -0300

    Implementado filtros de input e pontuação.

--- adivinhacao.py
+++ adivinhacao.py
@@ -6,18 +6,27 @@ print("**************************************")
 
 numero_secreto = random.randrange(1, 101)
 total_de_tentativas = 0
+pontos = 1000
+pontos_perdidos = 0
+verificador = False
 
-print("Qual o nível de dificuldade?")
-print("(1) Fácil (2) Médio (3) Difícil")
+while True:
+    try:
+        print("Qual o nível de dificuldade?")
+        print("(1) Fácil (2) Médio (3) Difícil")
+        nivel = input("Defina um nível: ")
 
-nivel = int(input("Defina um nível: "))
+        if not nivel.isnumeric():
+            raise ValueError("Apenas números são permitidos!")
 
-if nivel < 1 or nivel > 3:
-    print("Defina um nível entre 1 e 3!")
-    print("(1) Fácil (2) Médio (3) Difícil")
-    nivel = int(input("Defina um nível: "))
+        if int(nivel) < 1 or int(nivel) > 3:
+            raise ValueError("Nível fora do range permitido: 1 - 3!")
+    except ValueError as e:
+        print("Valor inválido:", e)
+    else:
+        break
 
-match nivel:
+match int(nivel):
     case 1:
         total_de_tentativas = 20
     case 2:
@@ -27,29 +36,44 @@ match nivel:
 
 for rodada in range(1, total_de_tentativas + 1):
     print(f"Tentativa: {str(rodada)} de {str(total_de_tentativas)}")
-    chute = input("Digite um número entre 1 e 100: ")
+    print(f"Pontos: {pontos} pontos")
 
-    if not chute.isnumeric():
-        print("Você deve digitar um número!")
-        continue
+    while True:
+        try:
+            chute = input("Digite um número entre 1 e 100: ")
 
-    if int(chute) < 1 or int(chute) > 100:
-        print("Você deve digitar um número entre 1 e 100!")
-        continue
+            if not chute.isnumeric():
+                raise ValueError("Você deve digitar apenas NÚMEROS entre 1 e 100!")
+
+            if int(chute) < 1 or int(chute) > 100:
+                raise ValueError("Você deve digitar um número entre 1 e 100!")
+
+        except ValueError as e:
+            print("Valor inválido: ", e)
+        else:
+            break
 
     acertou = int(chute) == numero_secreto
-    maior   = int(chute) > numero_secreto
-    menor   = int(chute) < numero_secreto
+    maior = int(chute) > numero_secreto
+    menor = int(chute) < numero_secreto
 
     print("Você digitou ", chute)
 
     if acertou:
-        print("Você acertou!")
+        print(f"Você acertou e fez {pontos} pontos!")
         break
     elif maior:
         print("Você errou! O seu chute foi maior do que o número secreto.")
+        pontos_perdidos = abs(numero_secreto - int(chute))
+        if rodada == total_de_tentativas:
+            print("O número secreto era {}. Você fez {}".format(numero_secreto, pontos))
     elif menor:
         print("Você errou! O seu chute foi menor do que o número secreto.")
+        pontos_perdidos = abs(numero_secreto - int(chute))
+        if rodada == total_de_tentativas:
+            print("O número secreto era {}. Você fez {} pontos.".format(numero_secreto, pontos))
+
+    pontos = pontos - pontos_perdidos
 
 print("**************************************")
 print("Fim de jogo!")
