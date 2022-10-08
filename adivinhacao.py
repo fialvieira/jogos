6dff7dec010d5756c9cc2df22bc881f45109e204
DESKTOP-O26PO1Q\fialv <fialvieira@gmail.com>
2022-10-08 17:03:09 -0300

    Implementados ajustes para chamar os jogos pelo terminal e também, através do jogos.py. ************************** if __name__ == "__main__":     escolher_jogo() **************************

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,80 +1,85 @@
 import random
 
-print("**************************************")
-print("Olá, bem vindo ao jogo de adivinhação!")
-print("**************************************")
 
-numero_secreto = random.randrange(1, 101)
-total_de_tentativas = 0
-pontos = 1000
-pontos_perdidos = 0
-verificador = False
+def jogar():
+    print("**************************************")
+    print("Olá, bem vindo ao jogo de adivinhação!")
+    print("**************************************")
 
-while True:
-    try:
-        print("Qual o nível de dificuldade?")
-        print("(1) Fácil (2) Médio (3) Difícil")
-        nivel = input("Defina um nível: ")
-
-        if not nivel.isnumeric():
-            raise ValueError("Apenas números são permitidos!")
-
-        if int(nivel) < 1 or int(nivel) > 3:
-            raise ValueError("Nível fora do range permitido: 1 - 3!")
-    except ValueError as e:
-        print("Valor inválido:", e)
-    else:
-        break
-
-match int(nivel):
-    case 1:
-        total_de_tentativas = 20
-    case 2:
-        total_de_tentativas = 10
-    case 3:
-        total_de_tentativas = 5
-
-for rodada in range(1, total_de_tentativas + 1):
-    print(f"Tentativa: {str(rodada)} de {str(total_de_tentativas)}")
-    print(f"Pontos: {pontos} pontos")
+    numero_secreto = random.randrange(1, 101)
+    total_de_tentativas = 0
+    pontos = 1000
+    pontos_perdidos = 0
 
     while True:
         try:
-            chute = input("Digite um número entre 1 e 100: ")
+            print("Qual o nível de dificuldade?")
+            print("(1) Fácil (2) Médio (3) Difícil")
+            nivel = input("Defina um nível: ")
 
-            if not chute.isnumeric():
-                raise ValueError("Você deve digitar apenas NÚMEROS entre 1 e 100!")
+            if not nivel.isnumeric():
+                raise ValueError("Apenas números são permitidos!")
 
-            if int(chute) < 1 or int(chute) > 100:
-                raise ValueError("Você deve digitar um número entre 1 e 100!")
-
+            if int(nivel) < 1 or int(nivel) > 3:
+                raise ValueError("Nível fora do range permitido: 1 - 3!")
         except ValueError as e:
-            print("Valor inválido: ", e)
+            print("Valor inválido:", e)
         else:
             break
 
-    acertou = int(chute) == numero_secreto
-    maior = int(chute) > numero_secreto
-    menor = int(chute) < numero_secreto
+    match int(nivel):
+        case 1:
+            total_de_tentativas = 20
+        case 2:
+            total_de_tentativas = 10
+        case 3:
+            total_de_tentativas = 5
 
-    print("Você digitou ", chute)
+    for rodada in range(1, total_de_tentativas + 1):
+        print(f"Tentativa: {str(rodada)} de {str(total_de_tentativas)}")
+        print(f"Pontos: {pontos} pontos")
 
-    if acertou:
-        print(f"Você acertou e fez {pontos} pontos!")
-        break
-    elif maior:
-        print("Você errou! O seu chute foi maior do que o número secreto.")
-        pontos_perdidos = abs(numero_secreto - int(chute))
-        if rodada == total_de_tentativas:
-            print("O número secreto era {}. Você fez {}".format(numero_secreto, pontos))
-    elif menor:
-        print("Você errou! O seu chute foi menor do que o número secreto.")
-        pontos_perdidos = abs(numero_secreto - int(chute))
-        if rodada == total_de_tentativas:
-            print("O número secreto era {}. Você fez {} pontos.".format(numero_secreto, pontos))
+        while True:
+            try:
+                chute = input("Digite um número entre 1 e 100: ")
 
-    pontos = pontos - pontos_perdidos
+                if not chute.isnumeric():
+                    raise ValueError("Você deve digitar apenas NÚMEROS entre 1 e 100!")
 
-print("**************************************")
-print("Fim de jogo!")
-print("**************************************")
+                if int(chute) < 1 or int(chute) > 100:
+                    raise ValueError("Você deve digitar um número entre 1 e 100!")
+
+            except ValueError as e:
+                print("Valor inválido: ", e)
+            else:
+                break
+
+        acertou = int(chute) == numero_secreto
+        maior = int(chute) > numero_secreto
+        menor = int(chute) < numero_secreto
+
+        print("Você digitou ", chute)
+
+        if acertou:
+            print(f"Você acertou e fez {pontos} pontos!")
+            break
+        elif maior:
+            print("Você errou! O seu chute foi maior do que o número secreto.")
+            pontos_perdidos = abs(numero_secreto - int(chute))
+            if rodada == total_de_tentativas:
+                print("O número secreto era {}. Você fez {}".format(numero_secreto, pontos))
+        elif menor:
+            print("Você errou! O seu chute foi menor do que o número secreto.")
+            pontos_perdidos = abs(numero_secreto - int(chute))
+            if rodada == total_de_tentativas:
+                print("O número secreto era {}. Você fez {} pontos.".format(numero_secreto, pontos))
+
+        pontos = pontos - pontos_perdidos
+
+    print("**************************************")
+    print("Fim de jogo!")
+    print("**************************************")
+
+
+if __name__ == "__main__":
+    jogar()
---
+++ jogos.py
@@ -0,0 +1,35 @@
+import forca
+import adivinhacao
+
+
+def escolher_jogo():
+    print("**************************************")
+    print("*********Escolha o seu jogo!**********")
+    print("**************************************")
+
+    print("(1) Forca (2) Adivinhação")
+
+    while True:
+        try:
+            jogo = input("Qual o jogo? ")
+
+            if not jogo.isnumeric():
+                raise ValueError("Apenas números são permitidos!")
+
+            if int(jogo) < 1 or int(jogo) > 2:
+                raise ValueError("Nível fora do range permitido: 1 - 2!")
+        except ValueError as e:
+            print("Valor inválido:", e)
+        else:
+            break
+
+    if jogo == 1:
+        print("Jogando forca...")
+        forca.jogar()
+    else:
+        print("Jogando adivinhação...")
+        adivinhacao.jogar()
+
+
+if __name__ == "__main__":
+    escolher_jogo()
