cd460a71a7f8bef34d07df63ffdb7d81c3ba283a
DESKTOP-O26PO1Q\fialv <fialvieira@gmail.com>
2023-11-14 16:36:55 -0300

    letras_acertadas com FOR

--- forca.py
+++ forca.py
@@ -3,32 +3,46 @@ def jogar():
     print("***Olá, bem vindo ao jogo de forca!***")
     print("**************************************")
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    palavra_secreta = "banana".upper()
+    letras_acertadas = []
     enforcou = False
     acertou = False
+    erros = 0
 
-    while not enforcou and not acertou:
+    for letra in palavra_secreta:
+        letras_acertadas.append("_")
+
+    while (not enforcou and not acertou):
         letras_faltando = str(letras_acertadas.count('_'))
         print(f"Faltam {letras_faltando} letras para acertar!!!")
         print(''.join(letras_acertadas))
         chute = input("Qual a letra? ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if chute.upper() == letra.upper():
-                letras_acertadas[index] = letra
-            index += 1
+        if (chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if chute == letra:
+                    letras_acertadas[index] = letra
+                index += 1                
+        else:
+            erros += 1
+            print("Ops, você errou! Faltam {} tentativas.".format(len(palavra_secreta) - erros))
 
-        if ''.join(letras_acertadas) == palavra_secreta:
-            print(''.join(letras_acertadas))
-            print("Parabêns, você acertou!")
-            break;
+        enforcou = erros >= len(palavra_secreta)
+        acertou = "_" not in letras_acertadas
 
+    if (acertou):
+        print(''.join(letras_acertadas))
+        print("Parabêns, você acertou!")
+
+    if (enforcou):
+        print(''.join(letras_acertadas))
+        print("Que pena, você não acertou, tente novamente!")
+        
     print("**************************************")
     print("************Fim de jogo!**************")
     print("**************************************")
 
-if __name__ == "__main__":
+if (__name__ == "__main__"):
     jogar()
